55bfb10203701fbed97cdef3d452f84afdbd60d5
hasret keskin <hasret_1989_@hotmail.com>
2021-05-26 22:57:57 +0300

    proje dosyaları

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App(){
+    return( 
+        <div>
+            <h1>İlk React Uygulamam</h1>
+            <p>Hoşgeldiniz</p>
+        </div>
+    )
+}
+
+export default App;
